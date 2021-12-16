550f5e952a418b240dda8218ac190014a40be4dc
Liam Vlaskamp <lvlaskamp@gmail.com>
2021-12-16 17:27:24 +0100

    setup foundation login system

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 </head>
 <body class="text-center">
     <main class="form-signin">
-        <form>
+        <form action="authenticate.php" method="post">
             <img class="mb-4" src="images/placeholder.svg" alt="Company logo">
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <input type="text" name="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
                 <label for="floatingPassword">Password</label>
             </div>
 
